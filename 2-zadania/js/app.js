83dd12261390f194433e1cdc0074cca9c52bd8dd
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2020-01-06 16:54:52 +0100

    Add done exercises from second folder

--- 2-zadania/js/app.js
+++ 2-zadania/js/app.js
@@ -2,33 +2,32 @@ function MyObj() {
     this.pets = ["Pies", "Kot", "Świnka"];
 }
 
-MyObj.prototype.printNormal = function() {
+MyObj.prototype.printNormal = function () {
     console.log(this);
-    this.pets.forEach(function(el) {
+    this.pets.forEach(function (el) {
         console.log(el);
     });
 };
 
-MyObj.prototype.printBig = function() {
+MyObj.prototype.printBig = function () {
     console.log(this);
-    this.pets.forEach(function(el) {
-        console.log(el);
+    this.pets.forEach(function (el) {
+        console.log(el.toUpperCase());
     });
 };
 
-MyObj.prototype.printSmall = function() {
+MyObj.prototype.printSmall = function () {
     console.log(this);
-    this.pets.forEach(function(el) {
-        console.log(el);
+    this.pets.forEach(function (el) {
+        console.log(el.toLowerCase());
     });
 };
 
-MyObj.prototype.bindBtn = function() {
-    document.querySelector('#button1').addEventListener("click", this.printBig);
-    document.querySelector('#button2').addEventListener("click", this.printSmall);
-    document.querySelector('#button3').addEventListener("click", function() {
-        this.printNormal();
-    });
+MyObj.prototype.bindBtn = function () {
+    // This function works only with new this setted by bind(this)
+    document.querySelector('#button1').addEventListener("click", this.printBig.bind(this));
+    document.querySelector('#button2').addEventListener("click", this.printSmall.bind(this));
+    document.querySelector('#button3').addEventListener("click", this.printNormal.bind(this));
 };
 
 const ob1 = new MyObj();
